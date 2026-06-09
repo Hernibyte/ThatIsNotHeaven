495734584eaa30e6d1724037312e4fc8c205264b
Hernan <gossa.hernan@gmail.com>
2026-06-09 00:50:57 -0300

    Initial project

---
+++ Source/ThatIsNotHeaven/ThatIsNotHeaven.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ThatIsNotHeaven.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThatIsNotHeaven, "ThatIsNotHeaven" );
